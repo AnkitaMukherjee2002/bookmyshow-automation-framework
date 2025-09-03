517f8a38b3e5eaba20d637abe782e8b99a5b7c8e
AnkitaMukherjee2002 <ankitas.udl@gmail.com>
2025-09-03 10:05:47 +0530

    Initail code

--- src/main/java/base/DriverSetup.java
+++ src/main/java/base/DriverSetup.java
@@ -9,23 +9,28 @@ import java.time.Duration;
 
 public class DriverSetup {
     private static WebDriver driver;
+
     public static WebDriver getDriver(String browser) {
         if (driver == null) {
             if (browser == null || browser.isEmpty()) {
                 browser = ConfigLoader.getProperty("browser");
             }
+
             switch (browser.toLowerCase()) {
                 case "firefox":
                     driver = new FirefoxDriver();
                     break;
+
                 case "edge":
                     driver = new EdgeDriver();
                     break;
+
                 case "chrome":
                 default:
                     driver = new ChromeDriver();
                     break;
             }
+
             driver.manage().window().maximize();
             driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(10));
         }
